858e8bf95c5d1556636f326b1287255bebcfbf99
Parker <parkertrowland@gmail.com>
2015-11-14 16:49:05 -0500

    Added date/timestamp to twit.java

--- Twit.java
+++ Twit.java
@@ -1,7 +1,11 @@
+import java.sql.Timestamp;
+import java.util.Date;
+
 public class Twit {
 	private String TwitText;
 	private String TwitPhotoPath;
 	private double TwitIdentifier;
+	private java.util.Date TwitDate;
 	
 	public String getTwitText() { return this.TwitText; }
 	public void setTwitText(String s) { this.TwitText = s; }
@@ -11,19 +15,27 @@ public class Twit {
 	
 	public double getTwitIdentifier() { return this.TwitIdentifier; }
 	public void setTwitIdentifier(double d) { this.TwitIdentifier = d; }
+
+	public Timestamp getTwitDate() { return new Timestamp(this.TwitDate.getTime()); }
 	
 	Twit() {
 		this.TwitText = "default";
 		this.TwitPhotoPath = " ";
+		this.TwitIdentifier = 0.0;
+		this.TwitDate = new java.util.Date();
 	}
 	
 	Twit(String t) {
 		this.TwitText = t;
 		this.TwitPhotoPath = " ";
+		this.TwitIdentifier = 0.0;
+		this.TwitDate = new java.util.Date();
 	}
 	
 	Twit(String t, String p) {
 		this.TwitText = t;
 		this.TwitPhotoPath = p;
+		this.TwitIdentifier = 0.0;
+		this.TwitDate = new java.util.Date();
 	}
 }

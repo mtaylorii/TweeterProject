3b3c887e6401eb4405928c177813d30b0f4092fb
Parker <parkertrowland@gmail.com>
2015-11-17 15:31:29 -0500

    Added to String (printAccoutn) function

--- Account.java
+++ Account.java
@@ -47,8 +47,25 @@ public class Account {
 	}
 	public ArrayList<Twit> getTwits() { return this.Twits; }
 	public ArrayList<Twit> getTwits(Account a) { return a.getTwits(); }
+	public String printTwitIdentifiers() { 
+		String result = "";
+		for (int i = 0; i < this.Twits.size(); ++i) result += Twits.get(i).getTwitIdentifier() + ",";
+		return result;
+	}
+	
 	public ArrayList<Account> getSubscribedTo() { return this.SubscribedTo; }
+	public String printSubscribedTo() { 
+		String result = "";
+		for (int i = 0; i < this.SubscribedTo.size(); ++i) result += SubscribedTo.get(i).getUserName() + ",";
+		return result;
+	}
+	
 	public ArrayList<Account> getSubscribers() { return this.Subscribers; }
+	public String printSubscribers() { 
+		String result = "";
+		for (int i = 0; i < this.Subscribers.size(); ++i) result += Subscribers.get(i).getUserName() + ",";
+		return result;
+	}
 	
 	public void subscribe(Account a) { this.SubscribedTo.add(a); }
 	public void unSubscribe(Account a) {
@@ -61,17 +78,31 @@ public class Account {
 		}
 	}
 	
+	public void printAccount() {
+		String result = this.getUserName() + ";";
+		result += this.getPassword() + ";";
+		result += this.getProfileText() + ";";
+		result += this.getProfilePhotoPath() + ";";
+		if (this.Twits != null) result += this.printTwitIdentifiers() + ";";
+		else result += ";";
+		if (this.SubscribedTo != null)result += this.printSubscribedTo() + ";";
+		else result += ";";
+		if (this.Subscribers != null) result += this.printSubscribers() + ";";
+		else result += ";";
+		System.out.print(result);
+	}
+	
 	Account() {
 		this.UserName = "default";
 		this.Password = "default";
-		this.ProfileText = " ";
-		this.ProfilePhotoPath = " ";
+		this.ProfileText = "";
+		this.ProfilePhotoPath = "";
 	}
 	
 	Account(String u, String p) {
 		this.UserName = u;
 		this.Password = p;
-		this.ProfileText = " ";
-		this.ProfilePhotoPath = " ";
+		this.ProfileText = "";
+		this.ProfilePhotoPath = "";
 	}
 }

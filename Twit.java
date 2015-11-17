1bfe9d5fd341f487d3feba8882a1e4e4eff72d8d
cohodges <chodges@mail.umw.edu>
2015-11-17 15:38:51 -0500

    Update Twit.java

--- Twit.java
+++ Twit.java
@@ -6,7 +6,10 @@ public class Twit {
 	private String TwitPhotoPath;
 	private double TwitIdentifier;
 	private java.util.Date TwitDate;
-	
+        private String TwitPoster;
+        private String TwitHashtag;
+        private String TwitRecipient;
+        
 	public String getTwitText() { return this.TwitText; }
 	public void setTwitText(String s) { this.TwitText = s; }
 	
@@ -17,7 +20,16 @@ public class Twit {
 	public void setTwitIdentifier(double d) { this.TwitIdentifier = d; }
 
 	public Timestamp getTwitDate() { return new Timestamp(this.TwitDate.getTime()); }
-	
+		
+        public String getTwitPoster() { return this.TwitPoster; }
+        public void setTwitPoster(String p) { this.TwitPoster = p; }
+
+        public String getTwitHashtag() { return this.TwitHashtag; }
+        public void setTwitHashtag(String h) { this.TwitHashtag = h; }
+        
+        public String getTwitRecipient() { return this.TwitRecipient; }
+        public void setTwitRecipient(String r) { this.TwitRecipient = r; }
+        
 	Twit() {
 		this.TwitText = "default";
 		this.TwitPhotoPath = " ";
@@ -37,5 +49,23 @@ public class Twit {
 		this.TwitPhotoPath = p;
 		this.TwitIdentifier = 0.0;
 		this.TwitDate = new java.util.Date();
+	}
+        
+        /**
+         * Working idea for a constructor to be used for most all twits posted
+         * by users that takes parameters for the twit's text, a photo path for
+         * an attached photo, a hashtag, a possible recipient of the twit via
+         * the at symbol, and the poster of the twit; if a twit is posted
+         * without a picture, hashtag, or recipient, we will put a default value
+         * in the parameter when we create the Twit.
+         */
+        Twit(String t, String p, String h, String r, String tp) {
+                this.TwitText = t;
+                this.TwitPhotoPath = " ";
+                this.TwitIdentifier = 0.0;
+                this.TwitDate = new java.util.Date();
+                this.TwitHashtag = h;
+                this.TwitRecipient = r;
+                this.TwitPoster = tp;
 	}
 }

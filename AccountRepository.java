2eb949bdefe4791fba23ebf57a87ac3a22e195ef
Parker <parkertrowland@gmail.com>
2015-11-17 15:57:08 -0500

    Added AccountRepository

--- AccountRepository.java
+++ AccountRepository.java
@@ -1,1 +1,20 @@
+import java.util.ArrayList;
 
+public class AccountRepository {
+	private ArrayList<Account> accounts = new ArrayList<Account>();
+	
+	public void addAccount(Account a) {
+		this.accounts.add(a);
+	}
+	
+	public void removeAccount(Account a) {
+		boolean found = false;
+		for (int i = 0; i < this.accounts.size() && !found; ++i) {
+			if (accounts.get(i).getUserName() == a.getUserName()) {
+				accounts.remove(i);
+				found = true;
+			}
+		}
+	}
+	
+}

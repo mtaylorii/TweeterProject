a41f4bb8e4a22bd99899059942697340586a76b7
cohodges <chodges@mail.umw.edu>
2015-11-19 22:06:59 -0500

    Update ComparatorByName.java

--- ComparatorByName.java
+++ ComparatorByName.java
@@ -1,7 +1,7 @@
 import java.util.Comparator;
 /**
  * The class below implements Java's comparator utility and defines how Account
- * objects will be sorted in a list.
+ * objects will be sorted in a list; they will be sorted in alphabetical order.
  */
 public class ComparatorByName implements Comparator<Account> {
     public int compare(Account a1, Account a2) {

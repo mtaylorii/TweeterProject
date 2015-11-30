ed204412edd1ee55125a204d51c3e108bea55266
chall <chall7@mail.umw.edu>
2015-11-29 21:42:31 -0500

    Updated: deleted unused methods.

--- Account.java
+++ Account.java
@@ -1,4 +1,4 @@
-package writingTester;
+//package writingTester;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -25,126 +25,6 @@ public class Account {
 	public void setProfilePhotoPath(String s) { this.ProfilePhotoPath = s; }
 	
 	public void postTwit(Twit t) { this.Twits.add(t); }
-	
-	public void postTwit(/*Twit t*/) {
-		Twit t = new Twit();
-		String userOption = "";
-		int convertedOption = -1;
-		Scanner userInput = new Scanner(System.in);
-		boolean isValid = false;
-		int timesPrompted = 0;
-		ArrayList<String> menuOptions = new ArrayList<String>();
-		String menu[] = new String[]{"Edit twit text", "Edit twit picture", "Edit twit visibility", "Post twit", "Back to main menu"};
-		for (int i = 0; i < menu.length; ++i) menuOptions.add(menu[i]);
-		
-		double newTwitIdentifier = Math.random();
-		boolean uniqueIdentifier = false;
-		while (!uniqueIdentifier) {
-			boolean taken = false;
-			newTwitIdentifier = Math.random();
-			for (int i = 0; i < Twits.size() && !taken; ++i) {
-				if (Twits.get(i).getTwitIdentifier() == newTwitIdentifier) taken = true;
-			}
-			if (!taken) uniqueIdentifier = true;
-		}
-		t.setTwitIdentifier(newTwitIdentifier);
-		
-		String imageFileName = Double.toString(newTwitIdentifier);
-		
-		do {
-			timesPrompted = 0;
-			isValid = false;
-			while (!isValid) {
-				isValid = false;
-				userOption = "";
-				convertedOption = -1;
-				if (timesPrompted > 0) System.out.println(printHeader("Incorrect option"));
-				System.out.println(printHeader("Please enter an option:"));
-				for (int i = 0; i < menuOptions.size(); ++i) System.out.println(i + " " + menuOptions.get(i));
-				System.out.print(">>> ");
-				userOption = userInput.next();
-				try {
-					convertedOption = Integer.parseInt(userOption);
-					if (convertedOption <= menuOptions.size()-1 && convertedOption > -1 ) isValid = true;
-				}
-				catch (NumberFormatException e) {
-					System.out.println(printHeader("Please enter a valid integer"));
-				}
-				if (!isValid) ++timesPrompted;
-			}
-			if (convertedOption == 0) {
-				System.out.println(printHeader("Please enter the twit text:"));
-				System.out.print(">>> ");
-				userInput.nextLine();
-				userOption = userInput.nextLine();
-				t.setTwitText(userOption);
-			}
-			else if (convertedOption == 1) {
-				System.out.println(printHeader("Please enter the twit picture file path:"));
-				System.out.print(">>> ");
-				userOption = userInput.next();
-				t.setTwitPhotoPath(userOption);
-			}
-			else if (convertedOption == 2) {
-				ArrayList<String> menuOptionsVisibility = new ArrayList<String>();
-				menu = null;
-				menu = new String[]{"Public (default)", "Subscribers only"};
-				for (int i = 0; i < menu.length; ++i) menuOptionsVisibility.add(menu[i]);
-				isValid = false;
-				timesPrompted = 0;
-				int convertedOption2 = -1;
-				while (!isValid) {
-					userOption = "";
-					convertedOption2 = -1;
-					if (timesPrompted > 0) System.out.println(printHeader("Incorrect option"));
-					System.out.println(printHeader("Please enter an option:"));
-					for (int i = 0; i < menuOptionsVisibility.size(); ++i) System.out.println(i + " " + menuOptionsVisibility.get(i));
-					System.out.print(">>> ");
-					userOption = userInput.next();
-					try {
-						convertedOption2 = Integer.parseInt(userOption);
-						if (convertedOption2 <= menuOptionsVisibility.size()-1 && convertedOption2 > -1 ) isValid = true;
-					}
-					catch (NumberFormatException e) {
-						System.out.println(printHeader("Please enter a valid integer"));
-					}
-					if (!isValid) ++timesPrompted;
-				}
-				if (convertedOption2 == 0) t.setTwitVisibility(true);
-				else if (convertedOption2 == 1) t.setTwitVisibility(false);
-			}
-			else if (convertedOption == 3) {
-				if (t.getTwitText().equals("~")) System.out.println(printHeader("Can not post an empty twit"));
-				else {
-					InputStream is = null;
-			        OutputStream os = null;
-			        String newFile = "";
-			        String filePath = t.getTwitPhotoPath();
-			        try {
-			            is = new FileInputStream(new File(filePath));
-			            String fileExtension = filePath.substring(filePath.length()-4);
-			            System.out.println("FILE EXT: " + fileExtension);
-			            System.out.println("FILE NAME: " + Double.toString(t.getTwitIdentifier()));
-			            newFile = this.getUserName() + Double.toString(t.getTwitIdentifier()) + fileExtension;
-			            t.setTwitPhotoPath(newFile);
-			            os = new FileOutputStream(new File(newFile));
-			            byte[] buffer = new byte[2048];
-			            int length;
-			            while ((length = is.read(buffer)) > 0) {
-			                os.write(buffer, 0, length);
-			            }
-			            is.close();
-			            os.close();
-			        }
-			          catch (IOException e) {
-			        	  //System.out.println(e);
-			        } finally {
-			        }
-					Twits.add(t);
-				}
-			}
-		} while(!(convertedOption == menuOptions.size()-1 || convertedOption == menuOptions.size()-2));
-	}
 	
 	public void postTwit2(/*Twit t*/) {
 		Twit t = new Twit();
@@ -242,16 +122,7 @@ public class Account {
 			}
 		} while(!(convertedOption == menuOptions.size()-1 || convertedOption == menuOptions.size()-2));
 	}
-	
-	public void deleteTwit(Twit t) {
-		boolean found = false;
-		for (int i = 0; i < this.Twits.size() && !found; ++i) {
-			if (Twits.get(i).getTwitIdentifier() == t.getTwitIdentifier()) {
-				Twits.remove(i);
-				found = true;
-			}
-		}
-	}
+
 	public ArrayList<Twit> getTwits() { return this.Twits; }
 	public ArrayList<Twit> getTwits(Account a) { return a.getTwits(); }
 	public String printTwitIdentifiers() { 
@@ -378,17 +249,7 @@ public class Account {
 		this.ProfileText = "";
 		this.ProfilePhotoPath = "";
 	}
-	
-	public void processTwits(String s) {
-		String delimiter = "[,]+";
-		String[] delimited = s.split(delimiter);
-		for (int i = 0; i < delimited.length; ++i) {
-			Twit tempTwit = new Twit();
-			double tempDouble = Double.parseDouble(delimited[i]);
-			tempTwit.setTwitIdentifier(tempDouble);
-			this.Twits.add(tempTwit);
-		}
-	}
+
 	
 	public void processSubscribedTo(String s) {
 		String delimiter = "[,]+";
@@ -411,101 +272,7 @@ public class Account {
 			this.Subscribers.add(tempAccount);
 		}
 	}
-	
-	Account(String u, String p, String pt, String ppp, String t, String pst, String ps) {
-		this.UserName = u;
-		this.Password = p;
-		this.ProfileText = pt;
-		this.ProfilePhotoPath = ppp;
-		String delimiter = "[,]+";
-		String[] delimited = t.split(delimiter);
-		for (int i = 0; i < delimited.length; ++i) {
-			Twit tempTwit = new Twit();
-			double tempDouble = Double.parseDouble(delimited[i]);
-			tempTwit.setTwitIdentifier(tempDouble);
-			this.Twits.add(tempTwit);
-		}
-		delimiter = "[,]+";
-		delimited = null;
-		delimited = pst.split(delimiter);
-		for (int i = 0; i < delimited.length; ++i) {
-			Account tempAccount = new Account();
-			String tempString = delimited[i];
-			tempAccount.setUserName(tempString);
-			this.SubscribedTo.add(tempAccount);
-		}
-		delimiter = "[,]+";
-		delimited = null;
-		delimited = ps.split(delimiter);
-		for (int i = 0; i < delimited.length; ++i) {
-			Account tempAccount = new Account();
-			String tempString = delimited[i];
-			tempAccount.setUserName(tempString);
-			this.Subscribers.add(tempAccount);
-		}
-	}
-	
-	public void editProfile() {
-		String userOption = "";
-		int convertedOption = -1;
-		Scanner userInput = new Scanner(System.in);
-		boolean isValid = false;
-		int timesPrompted = 0;
-		ArrayList<String> menuOptions = new ArrayList<String>();
-		String menu[] = new String[]{"Edit bio", "Edit profile picture", "Back to main menu"};
-		for (int i = 0; i < menu.length; ++i) menuOptions.add(menu[i]);
-			while (!isValid) {
-				userOption = "";
-				convertedOption = -1;
-				if (timesPrompted > 0) System.out.println(printHeader("Incorrect option"));
-				System.out.println(printHeader("Please enter an option:"));
-				for (int i = 0; i < menuOptions.size(); ++i) System.out.println(i + " " + menuOptions.get(i));
-				System.out.print(">>> ");
-				userOption = userInput.next();
-				try {
-					convertedOption = Integer.parseInt(userOption);
-					if (convertedOption <= menuOptions.size()-1 && convertedOption > -1 ) isValid = true;
-				}
-				catch (NumberFormatException e) {
-					System.out.println(printHeader("Please enter a valid integer"));
-				}
-				if (!isValid) ++timesPrompted;
-			}
-			if (convertedOption == 0) {
-				System.out.println(printHeader("Please enter a new profile biography:"));
-				System.out.print(">>> ");
-				userInput.nextLine();
-				userOption = userInput.nextLine();
-				this.setProfileText(userOption);
-			}
-			if (convertedOption == 1) {
-				System.out.println(printHeader("Please enter the profile picture file path:"));
-				System.out.print(">>> ");
-				userOption = userInput.next();
-				InputStream is = null;
-		        OutputStream os = null;
-		        String newFile = "";
-		        try {
-		            is = new FileInputStream(new File(userOption));
-		            String fileExtension = userOption.substring(userOption.length()-4);
-		            System.out.println("FILE EXT: " + fileExtension);
-		            newFile = this.getUserName() + fileExtension;
-		            os = new FileOutputStream(new File(newFile));
-		            byte[] buffer = new byte[2048];
-		            int length;
-		            while ((length = is.read(buffer)) > 0) {
-		                os.write(buffer, 0, length);
-		            }
-		            is.close();
-		            os.close();
-		        }
-		          catch (IOException e) {
-		        	  System.out.println(e);
-		        } finally {
-		        }
-				this.setProfilePhotoPath(newFile);
-			}
-	}
+
 	
 	public void editProfile2() {
 		String userOption = "";
